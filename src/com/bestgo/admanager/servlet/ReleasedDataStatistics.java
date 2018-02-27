8c9c949d0f70ce90355ba675269ef326800652da
zmjjobs <dream98job@126.com>
2018-02-27 14:47:37 +0800

    优化投放数据统计查询的SQL

--- src/com/bestgo/admanager/servlet/ReleasedDataStatistics.java
+++ src/com/bestgo/admanager/servlet/ReleasedDataStatistics.java
@@ -37,99 +37,79 @@ public class ReleasedDataStatistics extends HttpServlet {
         if (path.startsWith("/query_released_data_statistics")) {
             JsonArray jsonArray = new JsonArray();
             try {
-                String sqlG = "select id,team_name from web_ad_category_team";
-                if(!likeTeamName.isEmpty()){
-                    sqlG = "select id,team_name from web_ad_category_team where team_name like '%" + likeTeamName + "%'";
-                }
-                List<JSObject> listJS = DB.findListBySql(sqlG);
-                if(listJS != null && listJS.size()>0){
-                    for(JSObject oneG : listJS){
-                        if(oneG.hasObjectData()){
-                            long teamId = oneG.get("id");
-                            String teamName = oneG.get("team_name");
-                            sqlG = "select id,category_name from web_ad_tag_category where team_id = " + teamId;
-                            if(!likeCategoryName.isEmpty()){
-                                sqlG = "select id,category_name from web_ad_tag_category where team_id = " + teamId + " and category_name like '%" + likeCategoryName + "%'";
-                            }
-                            List<JSObject> listCategory = DB.findListBySql(sqlG);
-                            if(listCategory != null && listCategory.size()>0){
-                                for(JSObject j : listCategory){
-                                    if(j.hasObjectData()){
-                                        long categoryId = j.get("id");
-                                        String categoryName = j.get("category_name");
-                                        sqlG = "select t.id,t.tag_name,google_package_id,anticipated_incoming,anticipated_revenue from web_tag t LEFT JOIN web_facebook_app_ids_rel r ON t.tag_name = r.tag_name  where tag_category_id = " + categoryId;
-                                        List<JSObject> listTag = DB.findListBySql(sqlG);
-
-                                        if(listTag != null && listTag.size()>0){
-                                            for(JSObject t : listTag){
-                                                if(t.hasObjectData()){
-                                                    long tagId = t.get("id");
-                                                    String tagName = t.get("tag_name");
-                                                    double anticipatedIncoming = t.get("anticipated_incoming");
-                                                    double anticipatedRevenue = t.get("anticipated_revenue");
-                                                    JsonObject d = new JsonObject();
-                                                    d.addProperty("team_name",teamName);
-                                                    d.addProperty("category_name",categoryName);
-                                                    d.addProperty("tag_name",tagName);
-                                                    d.addProperty("anticipated_incoming",anticipatedIncoming);
-                                                    d.addProperty("anticipated_revenue",anticipatedRevenue);
-                                                    for(int i=0;i>-7;i--){
-                                                        String date = DateUtil.addDay(endTime,i,"yyyy-MM-dd");
-                                                        sqlG = "select sum(ch.total_spend) as spend " +
-                                                                " from web_ad_campaigns c, web_ad_campaigns_history ch, " +
-                                                                "(select distinct campaign_id from web_ad_campaign_tag_rel where tag_id = " + tagId + ") rt " +
-                                                                "where rt.campaign_id = ch.campaign_id and c.campaign_id = ch.campaign_id " +
-                                                                "and date = '" + date + "' " +
-                                                                "and c.status != 'removed'";
-                                                        JSObject x = DB.findOneBySql(sqlG);
-                                                        double totalSpend = 0;
-                                                        if(x.hasObjectData()){
-                                                            totalSpend = Utils.convertDouble(x.get("spend"),0);
-                                                        }
-                                                        sqlG = "select sum(ch.total_spend) as spend " +
-                                                                " from web_ad_campaigns_admob c, web_ad_campaigns_history_admob ch, " +
-                                                                "(select distinct campaign_id from web_ad_campaign_tag_admob_rel where tag_id = " + tagId + ") rt " +
-                                                                "where rt.campaign_id = ch.campaign_id and c.campaign_id = ch.campaign_id " +
-                                                                "and date = '" + date + "' " +
-                                                                "and c.status != 'removed'";
-                                                        x = DB.findOneBySql(sqlG);
-                                                        if(x.hasObjectData()){
-                                                            totalSpend += Utils.convertDouble(x.get("spend"),0);
-                                                        }
-                                                        double totalRevenue = 0;
-                                                        String google_package_id = t.get("google_package_id");
-                                                        if(google_package_id != null){
-                                                            sqlG =  "select sum(revenue) as revenues " +
-                                                                    "from web_ad_country_analysis_report_history where app_id = '"
-                                                                    + google_package_id + "' and date = '" + date + "'";
-                                                            JSObject oneR = DB.findOneBySql(sqlG);
-                                                            if(oneR != null){
-                                                                totalRevenue = Utils.convertDouble(oneR.get("revenues"),0);
-                                                            }
-                                                        }
-                                                        double totalIncoming = totalRevenue - totalSpend;
-                                                        d.addProperty("total_revenue" + i,Utils.trimDouble(totalRevenue,0));
-                                                        d.addProperty("total_spend" + i,Utils.trimDouble(totalSpend,0));
-                                                        d.addProperty("total_incoming" + i,Utils.trimDouble(totalIncoming,0));
-                                                    }
-                                                    jsonArray.add(d);
-                                                }
-                                                }
-                                        }
-
+                String sqlG = "select team_name,category_name,t.id,t.tag_name,anticipated_incoming,anticipated_revenue,google_package_id " +
+                        "from web_ad_category_team ct, web_ad_tag_category tc, web_tag t, web_facebook_app_ids_rel air " +
+                        "where tc.id = t.tag_category_id and ct.id = tc.team_id and t.tag_name = air.tag_name " +
+                        ((likeTeamName == "") ? " " : " and team_name like '%" + likeTeamName + "%' ") +
+                        ((likeCategoryName == "") ? " " : " and category_name like '%" + likeCategoryName + "%' ") +
+                        " ORDER BY ct.id,tc.id,t.id ";
+                java.lang.System.out.println(sqlG);
+                List<JSObject> listTag = DB.findListBySql(sqlG);
+                if (listTag != null && listTag.size() > 0) {
+                    for (JSObject t : listTag) {
+                        if (t.hasObjectData()) {
+//                            long teamId = t.get("team_id");
+                            String teamName = t.get("team_name");
+//                            long categoryId = t.get("category_id");
+                            String categoryName = t.get("category_name");
+                            if (!"".equals(t.get("id"))) {
+                                long tagId = t.get("id");
+                                JsonObject d = new JsonObject();
+                                d.addProperty("team_name", teamName);
+                                d.addProperty("category_name", categoryName);
+                                String tagName = t.get("tag_name");
+                                double anticipatedIncoming = t.get("anticipated_incoming");
+                                double anticipatedRevenue = t.get("anticipated_revenue");
+                                d.addProperty("tag_name", tagName);
+                                d.addProperty("anticipated_incoming", anticipatedIncoming);
+                                d.addProperty("anticipated_revenue", anticipatedRevenue);
+                                for (int i = 0; i > -7; i--) {
+                                    String date = DateUtil.addDay(endTime, i, "yyyy-MM-dd");
+                                    sqlG = "select sum(ch.total_spend) as spend " +
+                                            " from web_ad_campaigns c, web_ad_campaigns_history ch, " +
+                                            "(select distinct campaign_id from web_ad_campaign_tag_rel where tag_id = " + tagId + ") rt " +
+                                            "where rt.campaign_id = ch.campaign_id and c.campaign_id = ch.campaign_id " +
+                                            "and date = '" + date + "' " +
+                                            "and c.status != 'removed'";
+                                    JSObject x = DB.findOneBySql(sqlG);
+                                    double totalSpend = 0;
+                                    if (x.hasObjectData()) {
+                                        totalSpend = Utils.convertDouble(x.get("spend"), 0);
                                     }
+                                    sqlG = "select sum(ch.total_spend) as spend " +
+                                            " from web_ad_campaigns_admob c, web_ad_campaigns_history_admob ch, " +
+                                            "(select distinct campaign_id from web_ad_campaign_tag_admob_rel where tag_id = " + tagId + ") rt " +
+                                            "where rt.campaign_id = ch.campaign_id and c.campaign_id = ch.campaign_id " +
+                                            "and date = '" + date + "' " +
+                                            "and c.status != 'removed'";
+                                    x = DB.findOneBySql(sqlG);
+                                    if (x.hasObjectData()) {
+                                        totalSpend += Utils.convertDouble(x.get("spend"), 0);
+                                    }
+                                    double totalRevenue = 0;
+                                    String google_package_id = t.get("google_package_id");
+                                    if (google_package_id != "") {
+                                        sqlG = "select sum(revenue) as revenues " +
+                                                "from web_ad_country_analysis_report_history where app_id = '"
+                                                + google_package_id + "' and date = '" + date + "'";
+                                        JSObject oneR = DB.findOneBySql(sqlG);
+                                        if (oneR != null) {
+                                            totalRevenue = Utils.convertDouble(oneR.get("revenues"), 0);
+                                        }
+                                    }
+                                    double totalIncoming = totalRevenue - totalSpend;
+                                    d.addProperty("total_revenue" + i, Utils.trimDouble(totalRevenue, 0));
+                                    d.addProperty("total_spend" + i, Utils.trimDouble(totalSpend, 0));
+                                    d.addProperty("total_incoming" + i, Utils.trimDouble(totalIncoming, 0));
                                 }
-
+                                jsonArray.add(d);
                             }
 
                         }
-
                     }
-
-                    jsonObject.add("array", jsonArray);
-                    jsonObject.addProperty("ret", 1);
                 }
-
+                jsonObject.add("array", jsonArray);
+                jsonObject.addProperty("ret", 1);
             } catch (Exception e) {
                 jsonObject.addProperty("ret", 0);
                 jsonObject.addProperty("message", e.getMessage());
