2c31abca3107b007c8d9b69e14d3a4955a3f8d38
zmjjobs <dream98job@126.com>
2018-06-06 17:13:55 +0800

    再次修改一下Adwords名称规则

--- src/com/bestgo/admanager/servlet/CampaignAdmob.java
+++ src/com/bestgo/admanager/servlet/CampaignAdmob.java
@@ -166,11 +166,11 @@ public class CampaignAdmob extends HttpServlet {
                                 for(int i=0;i<createCountInt;i++){
                                     String now  = String.format("%d-%02d-%02d %02d:%02d:%02d", calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH) + 1, calendar.get(Calendar.DAY_OF_MONTH),
                                             calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), calendar.get(Calendar.SECOND));
-                                    String r = String.valueOf(random.nextInt());
-                                    String r_new = new StringBuffer(r).reverse().toString() ;
-                                    String s = String.valueOf(System.currentTimeMillis());
-                                    String s_new = new StringBuffer(s).reverse().toString();
-                                    campaignName = campaignNameOld + accountNameArr[j] + "_"+ i + "_" + r_new + "_" + s_new;
+                                    int r = random.nextInt();
+                                    long s = System.currentTimeMillis();
+                                    String part = new StringBuffer(i + r + s + "").reverse().toString();
+                                    campaignName = campaignNameOld + accountNameArr[j] + "_"+ (i + j) + part;
+
                                     if (campaignName.length() > 100) {
                                         campaignName = campaignName.substring(0, 100);
                                     }
