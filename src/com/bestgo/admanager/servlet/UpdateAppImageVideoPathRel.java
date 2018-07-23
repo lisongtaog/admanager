6caf10422ef908a5be0a4c39d77fb0b0bfd8e7ee
lisongtao <1471444606@qq.com>
2018-07-23 11:01:08 +0800

    解决创建广告页面，图片路径、视频路径错误显示的bug

--- src/com/bestgo/admanager/servlet/UpdateAppImageVideoPathRel.java
+++ src/com/bestgo/admanager/servlet/UpdateAppImageVideoPathRel.java
@@ -5,7 +5,6 @@ import com.bestgo.common.database.services.DB;
 import com.bestgo.common.database.utils.JSObject;
 import com.google.gson.JsonObject;
 
-import javax.rmi.CORBA.Util;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,67 +13,94 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-@WebServlet(name="UpdateAppImageVideoPathRel",urlPatterns = "/update_app_material_path_rel")
+@WebServlet(name = "UpdateAppImageVideoPathRel", urlPatterns = "/update_app_material_path_rel")
 public class UpdateAppImageVideoPathRel extends HttpServlet {
-    protected  void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException,ServletException{
-        doPost(request,response);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        doPost(request, response);
     }
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String appName = request.getParameter("app_name");
         JsonObject json = new JsonObject();
-        int err =0;
+        int err = 0;
         String message = "APP广告素材路径刷新成功";
         String sql = "SELECT config_key,config_value from web_system_config where config_key in ('fb_image_path','fb_video_path')";
         try {
             List<JSObject> configList = DB.findListBySql(sql);
-            if (configList.size()>0){
-                for (JSObject config: configList){
+            if (configList.size() > 0) {
+                for (JSObject config : configList) {
                     String configKey = config.get("config_key");
-                    if ("fb_image_path".equals(configKey)){
+                    if ("fb_image_path".equals(configKey)) {
                         String ParentPath = config.get("config_value"); // ParentPath 里是表格里存的根路径
-                        ParentPath = ParentPath.replace("/",File.separator);
+                        ParentPath = ParentPath.replace("/", File.separator);
 //                        ParentPath = ParentPath.replace("\\",File.separator);
                         String appParentPath = ParentPath + File.separatorChar + appName;
                         File file = new File(appParentPath);
                         List<String> PathList = Utils.ergodicImageDirectory(file, new ArrayList<>(), false);
 
-                        if(PathList.size() > 0){
+                        if (PathList.size() > 0) {
+                            Set<String> set = new HashSet<>();
                             DB.delete("ad_app_image_path_rel").where(DB.filter().whereEqualTo("app_name", appName)).execute();
-                            for(String imagePath : PathList){
-                                String imageRelativePath = imagePath.replace(ParentPath + File.separatorChar,"");
+                            for (String imagePath : PathList) {
+                                String imageRelativePath = imagePath.replace(ParentPath + File.separatorChar, "");
 
-                                try{
-                                    DB.insert("ad_app_image_path_rel")
-                                            .put("app_name",appName)
-                                            .put("image_path",imageRelativePath)
-                                            .execute();
-                                }catch(Exception e){
+                                try {
+
+                                  int index = imageRelativePath.lastIndexOf("\\");
+
+//                                    int index = imageRelativePath.lastIndexOf("/");
+                                    imageRelativePath = imageRelativePath.substring(0, index);
+
+                                    int oldSize = set.size();
+                                    set.add(imageRelativePath);
+                                    int newSize = set.size();
+                                    if (newSize > oldSize) {
+                                        DB.insert("ad_app_image_path_rel")
+                                                .put("app_name", appName)
+                                                .put("image_path", imageRelativePath)
+                                                .execute();
+                                    }
+                                } catch (Exception e) {
                                     message = e.getMessage();
                                     err++;
                                     e.printStackTrace();
                                 }
                             }
                         }
-                    }else{
+                    } else {
                         // "fb_video_path"的情况
                         String ParentPath = config.get("config_value");
-                        ParentPath = ParentPath.replace("/",File.separator);
+                        ParentPath = ParentPath.replace("/", File.separator);
 //                        ParentPath = ParentPath.replace("\\",File.separator);
                         String appParentPath = ParentPath + File.separatorChar + appName;
                         File file = new File(appParentPath);
                         List<String> PathList = Utils.ergodicVideoDirectory(file, new ArrayList<>(), false);
-                        if(PathList.size() > 0){
+                        if (PathList.size() > 0) {
+                            Set<String> set = new HashSet<>();
                             DB.delete("ad_app_video_path_rel").where(DB.filter().whereEqualTo("app_name", appName)).execute();
-                            for(String videoPath : PathList){
-                                String videoRelativePath = videoPath.replace(ParentPath + File.separatorChar,"");
-                                try{
-                                    DB.insert("ad_app_video_path_rel")
-                                            .put("app_name",appName)
-                                            .put("video_path",videoRelativePath)
-                                            .execute();
-                                }catch(Exception e){
+                            for (String videoPath : PathList) {
+                                String videoRelativePath = videoPath.replace(ParentPath + File.separatorChar, "");
+                                try {
+                                    int index = videoRelativePath.lastIndexOf("\\");
+//                                    int index = videoRelativePath.lastIndexOf("/");
+
+                                    videoRelativePath = videoRelativePath.substring(0, index);
+
+                                    int oldSize = set.size();
+                                    set.add(videoRelativePath);
+                                    int newSize = set.size();
+                                    if (newSize > oldSize) {
+                                        DB.insert("ad_app_video_path_rel")
+                                                .put("app_name", appName)
+                                                .put("video_path", videoRelativePath)
+                                                .execute();
+                                    }
+
+                                } catch (Exception e) {
                                     message = e.getMessage();
                                     err++;
                                     e.printStackTrace();
@@ -84,14 +110,14 @@ public class UpdateAppImageVideoPathRel extends HttpServlet {
                     }
                 }
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             message = e.getMessage();
             err = 0;
             e.printStackTrace();
         }
 
-        json.addProperty("message",message);
-        json.addProperty("err",err);
+        json.addProperty("message", message);
+        json.addProperty("err", err);
         response.setCharacterEncoding("UTF-8");
         response.getWriter().write(json.toString());
     }
